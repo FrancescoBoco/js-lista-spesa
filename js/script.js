3e1491788c68bb18dfbc1d40e9133517ab1aa05c
FrancescoBoco <francesco.bocoux@gmail.com>
2023-06-19 18:17:30 +0200

    end

--- js/script.js
+++ js/script.js
@@ -6,16 +6,43 @@
         1) CREARE UNA VARIABILE INDEX
         2) CREARE LA CONDIZIONE, FINCHE' INDEX < DI LIST.LEIGHT.
         3) AGGIUNGERE +1 A INDEX 
-        4) CODICE DA ESEGUIRE --> STAMPARE LIST[INDEX]
+        4) CODICE DA ESEGUIRE --> STAMPARE in console LIST[INDEX]
+ 3) CREARE HTML 
+ 4) AGGIUNGERE LA STANPA NELL'HTML 
+        - USARE APPEND 
 */
 
-const list= [
+const list = [
     'latte',
     'pane',
     'zucchine',
     'acqua',
     'cocacola'
 ];
+
+let index = 0
+
+const shoppingList = document.getElementById('shopping-list')
+
+
+while (index < list.length){
+
+    console.log ('lista spesa', index, list[index])
+
+    // shoppingList.append(myLi + list[index])
+    const myLi = document.createElement('li') 
+    // myLi.innerHTML = list[index];
+    // 
+    shoppingList.append(myLi)
+    myLi.append(list[index])
+
+    index++
+
+};
+
+
+
+
 
 
 
