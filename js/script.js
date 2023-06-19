198090d8d6f57a84dd863529cc892d3848f3a0cd
FrancescoBoco <francesco.bocoux@gmail.com>
2023-06-19 17:31:47 +0200

    ADD brain storming

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 
 /*
  LISTA DELLA SPESA CON CILO WHILE 
+ 1) CREARE UN ARRY 
+ 2) CREARE UN CILO WHILE 
+        1) CREARE UNA VARIABILE INDEX
+        2) CREARE LA CONDIZIONE, FINCHE' INDEX < DI LIST.LEIGHT.
+        3) AGGIUNGERE +1 A INDEX 
+        4) CODICE DA ESEGUIRE --> STAMPARE LIST[INDEX]
 */
 
 const list= [
@@ -10,4 +16,6 @@ const list= [
     'acqua',
     'cocacola'
 ];
+
+
 
